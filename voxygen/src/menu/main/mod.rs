870321df7db7fa96f60781a42823b4ba4fa81098
Imbris <imbrisf@gmail.com>
2020-06-06 00:37:29 -0400

    Make cancel button in connecting screen visble again, don't save username & server for singleplayer login, make iced version go back to login screen properly.

--- voxygen/src/menu/main/mod.rs
+++ voxygen/src/menu/main/mod.rs
@@ -214,6 +214,15 @@ impl PlayState for MainMenuState {
                     password,
                     server_address,
                 } => {
+                    let mut net_settings = &mut global_state.settings.networking;
+                    net_settings.username = username.clone();
+                    if !net_settings.servers.contains(&server_address) {
+                        net_settings.servers.push(server_address.clone());
+                    }
+                    if let Err(err) = global_state.settings.save_to_file() {
+                        warn!("Failed to save settings: {:?}", err);
+                    }
+
                     attempt_login(
                         global_state,
                         username,
@@ -293,15 +302,6 @@ fn attempt_login(
     server_port: u16,
     client_init: &mut Option<ClientInit>,
 ) {
-    let mut net_settings = &mut global_state.settings.networking;
-    net_settings.username = username.clone();
-    if !net_settings.servers.contains(&server_address) {
-        net_settings.servers.push(server_address.clone());
-    }
-    if let Err(e) = global_state.settings.save_to_file() {
-        warn!(?e, "Failed to save settings");
-    }
-
     if comp::Player::alias_is_valid(&username) {
         // Don't try to connect if there is already a connection in progress.
         if client_init.is_none() {
