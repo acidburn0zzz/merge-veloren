b7e5d48e064fbe2ff0115c1f11663a44ce2c8a6c
Capucho <jcapucho7@gmail.com>
2020-09-13 11:03:42 +0100

    Finished porting all pipelines

--- voxygen/src/render/pipelines/postprocess.rs
+++ voxygen/src/render/pipelines/postprocess.rs
@@ -1,33 +1,25 @@
-use super::super::{Mesh, Tri};
+use super::super::{AaMode, GlobalsLayouts, Mesh, Tri};
 use zerocopy::AsBytes;
-
-// gfx_defines! {
-//     vertex Vertex {
-//         pos: [f32; 2] = "v_pos",
-//     }
-
-//     constant Locals {
-//         nul: [f32; 4] = "nul",
-//     }
-
-//     pipeline pipe {
-//         vbuf: gfx::VertexBuffer<Vertex> = (),
-
-//         locals: gfx::ConstantBuffer<Locals> = "u_locals",
-//         globals: gfx::ConstantBuffer<Globals> = "u_globals",
-
-//         src_sampler: gfx::TextureSampler<<WinColorFmt as
-// gfx::format::Formatted>::View> = "src_color",
-
-//         tgt_color: gfx::RenderTarget<WinColorFmt> = "tgt_color",
-//         tgt_depth: gfx::DepthTarget<WinDepthFmt> =
-// gfx::preset::depth::PASS_TEST,     }
-// }
 
 #[repr(C)]
 #[derive(Copy, Clone, Debug, AsBytes)]
 pub struct Vertex {
     pub pos: [f32; 2],
+}
+
+impl Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferDescriptor<'a> {
+        use std::mem;
+        wgpu::VertexBufferDescriptor {
+            stride: mem::size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::InputStepMode::Vertex,
+            attributes: &[wgpu::VertexAttributeDescriptor {
+                offset: 0,
+                shader_location: 0,
+                format: wgpu::VertexFormat::Float2,
+            }],
+        }
+    }
 }
 
 pub fn create_mesh() -> Mesh<Vertex> {
@@ -48,4 +40,118 @@ pub fn create_mesh() -> Mesh<Vertex> {
     ));
 
     mesh
+}
+
+pub struct PostProcessLayout {
+    pub src_color: wgpu::BindGroupLayout,
+}
+
+impl PostProcessLayout {
+    pub fn new(device: &wgpu::Device) -> Self {
+        Self {
+            src_color: device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: None,
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStage::VERTEX | wgpu::ShaderStage::FRAGMENT,
+                        ty: wgpu::BindingType::SampledTexture {
+                            component_type: wgpu::TextureComponentType::Float,
+                            dimension: wgpu::TextureViewDimension::D2,
+                            multisampled: false,
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStage::VERTEX | wgpu::ShaderStage::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler { comparison: false },
+                        count: None,
+                    },
+                ],
+            }),
+        }
+    }
+}
+
+pub struct PostProcessPipeline {
+    pub pipeline: wgpu::RenderPipeline,
+}
+
+impl PostProcessPipeline {
+    pub fn new(
+        device: &wgpu::Device,
+        vs_module: &wgpu::ShaderModule,
+        fs_module: &wgpu::ShaderModule,
+        sc_desc: &wgpu::SwapChainDescriptor,
+        global_layout: &GlobalsLayouts,
+        layout: &PostProcessLayout,
+        aa_mode: AaMode,
+    ) -> Self {
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Post process pipeline layout"),
+                push_constant_ranges: &[],
+                bind_group_layouts: &[&global_layout.globals, &layout.src_color],
+            });
+
+        let samples = match aa_mode {
+            AaMode::None | AaMode::Fxaa => 1,
+            // TODO: Ensure sampling in the shader is exactly between the 4 texels
+            AaMode::SsaaX4 => 1,
+            AaMode::MsaaX4 => 4,
+            AaMode::MsaaX8 => 8,
+            AaMode::MsaaX16 => 16,
+        };
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Post process pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex_stage: wgpu::ProgrammableStageDescriptor {
+                module: vs_module,
+                entry_point: "main",
+            },
+            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+                module: fs_module,
+                entry_point: "main",
+            }),
+            rasterization_state: Some(wgpu::RasterizationStateDescriptor {
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: wgpu::CullMode::Back,
+                clamp_depth: false,
+                depth_bias: 0,
+                depth_bias_slope_scale: 0.0,
+                depth_bias_clamp: 0.0,
+            }),
+            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+            color_states: &[wgpu::ColorStateDescriptor {
+                format: sc_desc.format,
+                color_blend: wgpu::BlendDescriptor::REPLACE,
+                alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                write_mask: wgpu::ColorWrite::ALL,
+            }],
+            depth_stencil_state: Some(wgpu::DepthStencilStateDescriptor {
+                format: wgpu::TextureFormat::Depth24Plus,
+                depth_write_enabled: false,
+                depth_compare: wgpu::CompareFunction::Always,
+                stencil: wgpu::StencilStateDescriptor {
+                    front: wgpu::StencilStateFaceDescriptor::IGNORE,
+                    back: wgpu::StencilStateFaceDescriptor::IGNORE,
+                    read_mask: !0,
+                    write_mask: !0,
+                },
+            }),
+            vertex_state: wgpu::VertexStateDescriptor {
+                index_format: wgpu::IndexFormat::Uint16,
+                vertex_buffers: &[Vertex::desc()],
+            },
+            sample_count: samples,
+            sample_mask: !0,
+            alpha_to_coverage_enabled: false,
+        });
+
+        Self {
+            pipeline: render_pipeline,
+        }
+    }
 }
